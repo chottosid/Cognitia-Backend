749772c9486bfd380ab260400f5a20a912c60a82
Swarup Sidhartho Mondol <2005100@ugrad.cse.buet.ac.bd>
2025-07-04 01:23:48 +0600

    tasks

---
+++ prisma/migrations/20250703192315_1/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `notes` on the `tasks` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tasks" DROP COLUMN "notes";
